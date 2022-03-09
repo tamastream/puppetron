82254f374f010b7dc73a12d5765eae41f0ee46eb
letienne <loicetienne@gmail.com>
2022-03-09 21:22:38 +0100

    reduce chrome cpu usage
    https://stackoverflow.com/questions/49008008/chrome-headless-puppeteer-too-much-cpu

--- api/index.js
+++ api/index.js
@@ -147,6 +147,16 @@ async function handler(req, res) {
           ignoreHTTPSErrors: true,
           ...(isDev
             ? {
+                args: [
+                      '--no-sandbox',
+                      '--disable-setuid-sandbox',
+                      '--disable-dev-shm-usage',
+                      '--disable-accelerated-2d-canvas',
+                      '--no-first-run',
+                      '--no-zygote',
+                      '--single-process', // <- this one doesn't works in Windows
+                      '--disable-gpu'
+                    ],
                 headless: true,
                 executablePath: localChrome,
               }
